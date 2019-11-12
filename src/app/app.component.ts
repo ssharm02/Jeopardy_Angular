998fd34c6a87aa857211d23c9d58345862a43fd6
ssharm02 <sarthak.sharma02@gmail.com>
2019-11-12 15:02:24 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from "@angular/core";
+import { JeopardyService } from "src/services/getQuestions";
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  public jeoSub: Subscription;
+  public categories;
+  public allQuestions = new Array();
+  public GET_QUESTIONS = 4;
+  public category1 = new Object();
+  public category2 = new Object();
+  public category3 = new Object();
+  public category4 = new Object();
+  public category5 = new Object();
+  public jeopardyQuestion = new Array();
+  public possibleAnswers = new Array();
+
+  constructor(public jeotest: JeopardyService) {}
+
+  ngOnInit(): void {
+    this.manipulateObject();
+  }
+
+  getServiceData = async () => {
+    this.jeoSub = await this.jeotest.getItems().subscribe(async data => {
+      this.allQuestions.push(data);
+      console.log("all questions", this.allQuestions);
+
+      return this.allQuestions;
+    });
+  };
+
+  public manipulateObject() {
+    for (let i = 0; i <= this.GET_QUESTIONS; i++) {
+      this.getServiceData();
+    }
+    setTimeout(() => {
+      this.category1 = this.allQuestions[0];
+      this.category2 = this.allQuestions[1];
+      this.category3 = this.allQuestions[2];
+      this.category4 = this.allQuestions[3];
+      this.category5 = this.allQuestions[4];
+
+      console.log(this.category1);
+      this.filterCategories(this.category1);
+    }, 5000);
+  }
+
+  public filterCategories(data) {
+    data.filter((el) => {
+      this.jeopardyQuestion.push(el.question);
+      if (el.type === 'multiple') {
+        this.possibleAnswers.push(el.incorrect_answers + ',' + '  ' + el.correct_answer);
+        console.log('possible answers', this.possibleAnswers);
+      }
+    });
+
+  }
+}
---
+++ src/services/getQuestions.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+@Injectable()
+export class JeopardyService {
+  constructor(public http: HttpClient) {}
+  // The following methods return an observables
+  public categoryArray = [
+    32,
+    31,
+    29,
+    28,
+    27,
+    26,
+    25,
+    24,
+    22,
+    21,
+    20,
+    19,
+    18,
+    17,
+    16,
+    15,
+    14,
+    13,
+    12,
+    11,
+    10,
+    9
+  ];
+  public selectRandomCategory(maximum, minimum) {
+    let num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    this.categoryArray.splice(num, 1);
+    return num;
+  }
+
+  public getItems(): Observable<any> {
+    let category = this.selectRandomCategory(32, 9);
+    const NUMBER_OF_QUESTIONS = 5;
+    return this.http
+      .get(
+        `https://opentdb.com/api.php?amount=${NUMBER_OF_QUESTIONS}&category=${category}`
+      )
+      .pipe(map(data => data["results"]));
+  }
+}
