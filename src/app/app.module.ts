132f2a022b7866e4d47f9a11c7c9b9e17bad863e
Sarthak <sarthak.sharma02@gmail.com>
2019-11-12 21:12:20 -0500

    implemented boostrap, modular code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { JeopardyService } from "src/services/getQuestions";
 import { HttpClientModule } from "@angular/common/http";
+import { JeoQuestions } from 'src/questions/questions.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
+    declarations: [AppComponent, JeoQuestions],
   imports: [
     BrowserModule,
     HttpClientModule
