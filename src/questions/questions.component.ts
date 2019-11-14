c738459db7818810cc23d7bfcb3c886a33d5ae19
Sarthak <sarthak.sharma02@gmail.com>
2019-11-13 22:08:23 -0500

    minor updates

--- src/questions/questions.component.ts
+++ src/questions/questions.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-import { JeopardyService } from "src/services/getQuestions";
-import { Subscription } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { JeopardyService } from 'src/services/getQuestions';
+import { Subscription } from 'rxjs';
 @Component({
   selector: "jeo-questions",
-  templateUrl: "./questions.component.html",
-  styleUrls: ["./questions.component.css"]
+  templateUrl: './questions.component.html',
+  styleUrls: ['./questions.component.css']
 })
 export class JeoQuestions implements OnInit {
   public jeoSub: Subscription;
   public categories;
   public allQuestions = new Array();
   public GET_QUESTIONS = 4;
-  public clicked: boolean = false;
+  public clicked = false;
+  public dollarAmount;
+  public btnPressed: string;
+  public userChoice: string;
   public category1 = new Object();
   public category2 = new Object();
   public category3 = new Object();
@@ -30,12 +33,11 @@ export class JeoQuestions implements OnInit {
 
   getServiceData = async () => {
     this.jeoSub = await this.jeotest.getItems().subscribe(async data => {
-      this.allQuestions.push(data);
-      console.log("all questions", this.allQuestions);
 
+      this.allQuestions.push(data);
       return this.allQuestions;
     });
-  };
+  }
 
   public manipulateObject() {
     for (let i = 0; i <= this.GET_QUESTIONS; i++) {
@@ -47,46 +49,62 @@ export class JeoQuestions implements OnInit {
       this.category3 = this.allQuestions[2];
       this.category4 = this.allQuestions[3];
       this.category5 = this.allQuestions[4];
+
       this.mutateObject(this.category1);
       this.mutateObject(this.category2);
       this.mutateObject(this.category3);
       this.mutateObject(this.category4);
       this.mutateObject(this.category5);
-      console.log(this.category1);
-      this.filterCategories(this.category1);
     }, 5000);
   }
   public mutateObject(data) {
     data.forEach(e => {
       e.incorrect_answers.push(e.correct_answer);
     });
-    console.log("new data is ", data);
     return data;
   }
   public filterCategories(data) {
     data.filter(el => {
-      if (el.type === "multiple") {
+      if (el.type === 'multiple') {
         this.jeopardyQuestion.push(el.question);
-        this.possibleAnswers.push(
-          el.incorrect_answers + "," + " " + el.correct_answer
-        );
-        console.log("possible answers", this.possibleAnswers);
-        console.log("questions are ", this.jeopardyQuestion);
       }
     });
   }
 
   public userButtonClicked(event) {
-    console.log("button function launching");
-    const elementId: string = (event.target as Element).id;
-    const buttonValu = event.target.value;
-    console.log("button value is ", buttonValu);
-    console.log(elementId);
+    this.btnPressed = (event.target as Element).id;
+    // tslint:disable-next-line:radix
+    this.dollarAmount = parseInt(event.target.value);
     this.clicked = true;
   }
   public onSelectionChange(event) {
-    const userChoice = event.target.value;
-    console.log("selected Entry function launching ", userChoice);
-    return userChoice;
+    this.userChoice = event.target.value;
+  }
+  public checkAnswerGiveDollars() {
+
+    // tslint:disable-next-line: quotemark
+    if (this.btnPressed === "cat2-btn1") {
+      if (this.userChoice === this.category2[0].correct_answer) {
+        this.userScore += this.dollarAmount;
+      }
+    }
+    if (this.btnPressed === 'cat2-btn2') {
+      if (this.userChoice === this.category2[1].correct_answer) {
+        this.userScore += this.dollarAmount;
+      }
+    }
+    if (this.btnPressed === 'cat2-btn3') {
+      if (this.userChoice === this.category2[2].correct_answer) {
+        this.userScore += this.dollarAmount;
+      }
+    }
+    if (this.btnPressed === 'cat2-btn4') {
+      if (this.userChoice === this.category2[3].correct_answer) {
+      }
+    }
+    if (this.btnPressed === 'cat2-btn5') {
+      if (this.userChoice === this.category2[4].correct_answer) {
+      }
+    }
   }
 }
