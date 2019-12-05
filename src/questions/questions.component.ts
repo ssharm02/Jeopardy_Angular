aa202eb6591e2129f6aa801e26ac4e014c412535
Sarthak <sarthak.sharma02@gmail.com>
2019-12-04 22:46:16 -0500

    removed hardcoded button fields

--- src/questions/questions.component.ts
+++ src/questions/questions.component.ts
@@ -167,69 +167,27 @@ export class JeoQuestions implements OnInit, OnDestroy {
       Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
     return randomNum;
   }
+
+  public traverseCategories(categoryX, i) {
+    const categoryQuestion = categoryX[i].question;
+    const incorrectOptions = categoryX[i].incorrect_answers;
+    const disabledButton = categoryX[i].disabled;
+    return {
+      categoryQuestion,
+      incorrectOptions,
+      disabledButton
+    };
+  }
   public userButtonClicked(event): void {
-    const firstCategory1Q = this.category1[0].question;
-    const firstCategory1INQ = this.category1[0].incorrect_answers;
-    const firstCategory2Q = this.category1[1].question;
-    const firstCategory2INQ = this.category1[1].incorrect_answers;
-    const firstCategory3Q = this.category1[2].question;
-    const firstCategory3INQ = this.category1[2].incorrect_answers;
-    const firstCategory4Q = this.category1[3].question;
-    const firstCategory4INQ = this.category1[3].incorrect_answers;
-    const firstCategory5Q = this.category1[4].question;
-    const firstCategory5INQ = this.category1[4].incorrect_answers;
 
-    const secondCategory1Q = this.category2[0].question;
-    const secondCategory1INQ = this.category2[0].incorrect_answers;
-    const secondCategory2Q = this.category2[1].question;
-    const secondCategory2INQ = this.category2[1].incorrect_answers;
-    const secondCategory3Q = this.category2[2].question;
-    const secondCategory3INQ = this.category2[2].incorrect_answers;
-    const secondCategory4Q = this.category2[3].question;
-    const secondCategory4INQ = this.category2[3].incorrect_answers;
-    const secondCategory5Q = this.category2[4].question;
-    const secondCategory5INQ = this.category2[4].incorrect_answers;
-
-    const thirdCategory1Q = this.category3[0].question;
-    const thirdCategory1INQ = this.category3[0].incorrect_answers;
-    const thirdCategory2Q = this.category3[1].question;
-    const thirdCategory2INQ = this.category3[1].incorrect_answers;
-    const thirdCategory3Q = this.category3[2].question;
-    const thirdCategory3INQ = this.category3[2].incorrect_answers;
-    const thirdCategory4Q = this.category3[3].question;
-    const thirdCategory4INQ = this.category3[3].incorrect_answers;
-    const thirdCategory5Q = this.category3[4].question;
-    const thirdCategory5INQ = this.category3[4].incorrect_answers;
-
-    const fourthCategory1Q = this.category4[0].question;
-    const fourthCategory1INQ = this.category4[0].incorrect_answers;
-    const fourthCategory2Q = this.category4[1].question;
-    const fourthCategory2INQ = this.category4[1].incorrect_answers;
-    const fourthCategory3Q = this.category4[2].question;
-    const fourthCategory3INQ = this.category4[2].incorrect_answers;
-    const fourthCategory4Q = this.category4[3].question;
-    const fourthCategory4INQ = this.category4[3].incorrect_answers;
-    const fourthCategory5Q = this.category4[4].question;
-    const fourthCategory5INQ = this.category4[4].incorrect_answers;
-
-    const fifthCategory1Q = this.category5[0].question;
-    const fifthCategory1INQ = this.category5[0].incorrect_answers;
-    const fifthCategory2Q = this.category5[1].question;
-    const fifthCategory2INQ = this.category5[1].incorrect_answers;
-    const fifthCategory3Q = this.category5[2].question;
-    const fifthCategory3INQ = this.category5[2].incorrect_answers;
-    const fifthCategory4Q = this.category5[3].question;
-    const fifthCategory4INQ = this.category5[3].incorrect_answers;
-    const fifthCategory5Q = this.category5[4].question;
-    const fifthCategory5INQ = this.category5[4].incorrect_answers;
     this.navigateToScore();
     this.btnPressed = (event.target as Element).id;
     switch (this.btnPressed) {
       case "cat1-btn1":
         this.startTimer();
         this.modalId = "cat1";
-        this.cat = firstCategory1Q;
-        this.incorrectAnswers = firstCategory1INQ;
+        this.cat = this.traverseCategories(this.category1, 0).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category1, 0).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category1[0].disabled = true;
@@ -238,8 +196,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
       case "cat1-btn2":
         this.startTimer();
         this.modalId = "cat2";
-        this.cat = firstCategory2Q;
-        this.incorrectAnswers = firstCategory2INQ;
+        this.cat = this.traverseCategories(this.category1, 1).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category1, 1).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category1[1].disabled = true;
@@ -248,8 +206,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
       case "cat1-btn3":
         this.startTimer();
         this.modalId = "cat3";
-        this.cat = firstCategory3Q;
-        this.incorrectAnswers = firstCategory3INQ;
+        this.cat = this.traverseCategories(this.category1, 2).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category1, 2).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category1[2].disabled = true;
@@ -257,8 +215,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat1-btn4":
         this.modalId = "cat4";
-        this.cat = firstCategory4Q;
-        this.incorrectAnswers = firstCategory4INQ;
+        this.cat = this.traverseCategories(this.category1, 3).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category1, 3).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category1[3].disabled = true;
@@ -266,8 +224,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat1-btn5":
         this.modalId = "cat5";
-        this.cat = firstCategory5Q;
-        this.incorrectAnswers = firstCategory5INQ;
+        this.cat = this.traverseCategories(this.category1, 4).categoryQuestion;;
+        this.incorrectAnswers = this.traverseCategories(this.category1, 4).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category1[4].disabled = true;
@@ -275,8 +233,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat2-btn1":
         this.modalId = "cat1a";
-        this.cat = secondCategory1Q;
-        this.incorrectAnswers = secondCategory1INQ;
+        this.cat = this.traverseCategories(this.category2, 0).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category2, 0).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category2[0].disabled = true;
@@ -284,8 +242,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat2-btn2":
         this.modalId = "cat2a";
-        this.cat = secondCategory2Q;
-        this.incorrectAnswers = secondCategory2INQ;
+        this.cat = this.traverseCategories(this.category2, 1).categoryQuestion;;
+        this.incorrectAnswers = this.traverseCategories(this.category2, 1).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category2[1].disabled = true;
@@ -293,8 +251,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat2-btn3":
         this.modalId = "cat3a";
-        this.cat = secondCategory3Q;
-        this.incorrectAnswers = secondCategory3INQ;
+        this.cat = this.traverseCategories(this.category2, 2).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category2, 2).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category2[2].disabled = true;
@@ -302,8 +260,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat2-btn4":
         this.modalId = "cat4a";
-        this.cat = secondCategory4Q;
-        this.incorrectAnswers = secondCategory4INQ;
+        this.cat = this.traverseCategories(this.category2, 3).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category2, 3).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category2[3].disabled = true;
@@ -311,8 +269,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat2-btn5":
         this.modalId = "cat5a";
-        this.cat = secondCategory5Q;
-        this.incorrectAnswers = secondCategory5INQ;
+        this.cat = this.traverseCategories(this.category2, 4).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category2, 4).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category2[4].disabled = true;
@@ -320,8 +278,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat3-btn1":
         this.modalId = "cat1b";
-        this.cat = thirdCategory1Q;
-        this.incorrectAnswers = thirdCategory1INQ;
+        this.cat = this.traverseCategories(this.category3, 0).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category3, 0).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category3[0].disabled = true;
@@ -329,8 +287,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat3-btn2":
         this.modalId = "cat2b";
-        this.cat = thirdCategory2Q;
-        this.incorrectAnswers = thirdCategory2INQ;
+        this.cat = this.traverseCategories(this.category3, 1).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category3, 1).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category3[1].disabled = true;
@@ -338,8 +296,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat3-btn3":
         this.modalId = "cat3b";
-        this.cat = thirdCategory3Q;
-        this.incorrectAnswers = thirdCategory3INQ;
+        this.cat = this.traverseCategories(this.category3, 2).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category3, 2).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category3[2].disabled = true;
@@ -347,8 +305,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat3-btn4":
         this.modalId = "cat4b";
-        this.cat = thirdCategory4Q;
-        this.incorrectAnswers = thirdCategory4INQ;
+        this.cat = this.traverseCategories(this.category3, 3).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category3, 3).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category3[3].disabled = true;
@@ -356,8 +314,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat3-btn5":
         this.modalId = "cat5b";
-        this.cat = thirdCategory5Q;
-        this.incorrectAnswers = thirdCategory5INQ;
+        this.cat = this.traverseCategories(this.category3, 4).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category3, 4).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category3[4].disabled = true;
@@ -365,8 +323,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat4-btn1":
         this.modalId = "cat1aa";
-        this.cat = fourthCategory1Q;
-        this.incorrectAnswers = fourthCategory1INQ;
+        this.cat = this.traverseCategories(this.category4, 0).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category4, 0).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category4[0].disabled = true;
@@ -374,8 +332,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat4-btn2":
         this.modalId = "cat2aa";
-        this.cat = fourthCategory2Q;
-        this.incorrectAnswers = fourthCategory2INQ;
+        this.cat = this.traverseCategories(this.category4, 1).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category4, 1).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category4[1].disabled = true;
@@ -383,8 +341,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat4-btn3":
         this.modalId = "cat3aa";
-        this.cat = fourthCategory3Q;
-        this.incorrectAnswers = fourthCategory3INQ;
+        this.cat = this.traverseCategories(this.category4, 2).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category4, 2).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category4[2].disabled = true;
@@ -392,8 +350,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat4-btn4":
         this.modalId = "cat4aa";
-        this.cat = fourthCategory4Q;
-        this.incorrectAnswers = fourthCategory4INQ;
+        this.cat = this.traverseCategories(this.category4, 3).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category4, 3).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category4[3].disabled = true;
@@ -401,8 +359,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat4-btn5":
         this.modalId = "cat5aa";
-        this.cat = fourthCategory5Q;
-        this.incorrectAnswers = fourthCategory5INQ;
+        this.cat = this.traverseCategories(this.category4, 4).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category4, 4).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category4[4].disabled = true;
@@ -410,8 +368,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat5-btn1":
         this.modalId = "cat1bb";
-        this.cat = fifthCategory1Q;
-        this.incorrectAnswers = fifthCategory1INQ;
+        this.cat = this.traverseCategories(this.category5, 0).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category5, 0).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category5[0].disabled = true;
@@ -419,8 +377,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat5-btn2":
         this.modalId = "cat2bb";
-        this.cat = fifthCategory2Q;
-        this.incorrectAnswers = fifthCategory2INQ;
+        this.cat = this.traverseCategories(this.category5, 1).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category5, 1).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category5[1].disabled = true;
@@ -428,8 +386,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat5-btn3":
         this.modalId = "cat3bb";
-        this.cat = fifthCategory3Q;
-        this.incorrectAnswers = fifthCategory3INQ;
+        this.cat = this.traverseCategories(this.category5, 2).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category5, 2).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category5[2].disabled = true;
@@ -437,8 +395,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat5-btn4":
         this.modalId = "cat4bb";
-        this.cat = fifthCategory4Q;
-        this.incorrectAnswers = fifthCategory4INQ;
+        this.cat = this.traverseCategories(this.category5, 3).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category5, 3).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category5[3].disabled = true;
@@ -446,8 +404,8 @@ export class JeoQuestions implements OnInit, OnDestroy {
         break;
       case "cat5-btn5":
         this.modalId = "cat5bb";
-        this.cat = fifthCategory5Q;
-        this.incorrectAnswers = fifthCategory5INQ;
+        this.cat = this.traverseCategories(this.category5, 4).categoryQuestion;
+        this.incorrectAnswers = this.traverseCategories(this.category5, 4).incorrectOptions;
         this.questionCounter++;
         setTimeout(() => {
           this.category5[4].disabled = true;
