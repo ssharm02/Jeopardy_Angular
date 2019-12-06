a04f7268e45d140ecf544093b63764e780bc70f6
Sarthak <sarthak.sharma02@gmail.com>
2019-12-05 22:56:02 -0500

    class fix

--- src/models/JeopardyBoard.ts
+++ src/models/JeopardyBoard.ts
@@ -8,35 +8,39 @@ import { Observable, Subscription } from "rxjs";
 export class JeopardyBoard {
   public jeoSub: Subscription;
   public nameSub: Subscription;
-  public categories;
+  public subscription: Subscription;
+
   public showSpinner = true;
-  public allQuestions = new Array();
   public GET_QUESTIONS = 4;
   public questionCounter = 0;
-  public selectCategory: string;
   public dollarAmount = 0;
-  public cat: string;
-  public incorrectAnswers;
+  public userScore = 0;
+  public count = 60;
+
+  public selectCategory: string;
   public modalId: string;
   public btnPressed: string;
   public userChoice: string;
+
+  public cat = new Object();
   public category1 = new Object();
   public category2 = new Object();
   public category3 = new Object();
   public category4 = new Object();
   public category5 = new Object();
+  public incorrectAnswers = new Object();
   public allCategories = new Object();
-  public userScore = 0;
+
   public userAnswer: string;
+
+  public allQuestions = new Array();
   public jeopardyQuestion = new Array();
   public possibleAnswers = new Array();
-  public lastClicked;
-  name$: any;
-  subscription: Subscription;
+
+  public name$: any;
+  public counter$: Observable<number>;
   public timeLeft;
   public interval;
-  counter$: Observable<number>;
-  count = 60;
-  public dailyDoubleNum1;
-  public dailyDoubleNum2;
+  public dailyDoubleNum1: number;
+  public dailyDoubleNum2: number;
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,33 +1,3 @@
 export class User {
-    public categories;
-    public showSpinner = true;
-    public allQuestions = new Array();
-    public GET_QUESTIONS = 4;
-    public questionCounter = 0;
-    public selectCategory: string;
-    public dollarAmount = 0;
-    public cat: string;
-    public incorrectAnswers;
-    public modalId: string;
-    public btnPressed: string;
-    public userChoice: string;
-    public category1 = new Object();
-    public category2 = new Object();
-    public category3 = new Object();
-    public category4 = new Object();
-    public category5 = new Object();
-    public allCategories = new Object();
-    public userScore = 0;
-    public userAnswer: string;
-    public jeopardyQuestion = new Array();
-    public possibleAnswers = new Array();
-    public lastClicked;
-    name$: any;
-    public timeLeft;
-    public interval;
-    count = 60;
-    public dailyDoubleNum1;
-    public dailyDoubleNum2;
-constructor(public name: string, public score: number) {}
-
+  constructor(public name: string, public score: number) {}
 }
